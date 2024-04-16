e7c51e434603f54edde71e93f27fd3258f07f0e4
AndreyTara <explorituse@gmail.com>
2024-04-16 23:00:44 +0300

    'adds-h2-arr'

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,8 @@ console.log(`Number of categories ${categoriesUl.length}`);
 // Отримуємо посилання на елементи DOM всіх елементів "ul#categories li.item ul "
 const categoriesSublist = document.querySelectorAll("ul#categories li.item ul");
 // Отримуємо посилання на елементи DOM всіх елементів "ul#categories>li.item h2"
-const categoriesH2 = document.querySelectorAll("ul#categories li.item h2");
+const categoriesH2 = document.querySelectorAll("ul#categories>li.item>h2");
+console.log(categoriesH2);
 /**
  * getElements()  виведення порахованиї елементів в консоль
  */
@@ -14,7 +15,7 @@ function getElements() {
   const categoriesArr = [];
   categoriesSublist.forEach((item, index) => {
     categoriesArr.push(`Category: ${categoriesH2[index].textContent}`);
-    categoriesArr.push(`Elements: ${item.children.length} `);
+    categoriesArr.push(`Elements: ${item.children.length}`);
   });
 
   categoriesArr.forEach((el) => {
